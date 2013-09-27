8975700cbdee1274b95cff990583735a36b050d5
Jernej Čop <jernej.cop@agiledrop.com>
2013-09-27 15:16:51 +0200

    Another small fix

--- src/android/LockNavigation.java
+++ src/android/LockNavigation.java
@@ -37,8 +37,6 @@ public class LockNavigation extends CordovaPlugin {
                 // Set the needed variables
                 cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 cordova.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
-                System.out.println("Screen will be kept on. KeepScreenOn");
                 
               }
           });
